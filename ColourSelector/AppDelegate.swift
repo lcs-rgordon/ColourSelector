3b2599daeceeef96145124dfc14ca45ce47b0fec
Russell Gordon <rgordon@lcs.on.ca>
2020-02-08 08:49:07 -0500

    Initial Commit

---
+++ ColourSelector/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  ColourSelector
+//
+//  Created by Russell Gordon on 2020-02-08.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
