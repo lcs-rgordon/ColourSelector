81875f07beed518a77b8b112344915b5994eab4b
Russell Gordon <rgordon@lcs.on.ca>
2020-02-08 08:57:02 -0500

    Removed boilerplate code and set up scene to display primitives

---
+++ ColourSelector/PrimitivesScene.swift
@@ -0,0 +1,14 @@
+//
+//  PrimitivesScene.swift
+//  ColourSelector
+//
+//  Created by Russell Gordon on 2020-02-08.
+//  Copyright © 2020 Russell Gordon. All rights reserved.
+//
+
+import SceneKit
+import Cocoa
+
+class PrimitivesScene: SCNScene {
+
+}
